da0ca176ce6fcd09bc53041f68b1e8aae8055b2c
kirill.lastovirya <kirill.lastovirya@7300e6ac-da80-11dd-86d7-074df07e0730>
2009-01-07 07:49:28 +0000

    added html page to show fractals

--- trunk/src/org/kirhgoff/fractals/FractalPanel.java
+++ trunk/src/org/kirhgoff/fractals/FractalPanel.java
@@ -2,48 +2,28 @@ package org.kirhgoff.fractals;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JComponent;
-import javax.swing.JFrame;
+
+import org.kirhgoff.fractals.controllers.PaletteScroller;
+import org.kirhgoff.fractals.controllers.Zoomer;
 
 public class FractalPanel extends JComponent {
 
 	private static final long serialVersionUID = 1L;
-	protected Conversion conversion = new Conversion (getSize ());
-	protected Zoomer zoomer;
-	protected PaletteScroller paletteScroller = new PaletteScroller (this);
+	private Conversion conversion = new Conversion (getSize ());
+	private Zoomer zoomer  = new Zoomer (this);
+	private PaletteScroller paletteScroller = new PaletteScroller (this);
 	private Image image = null;
-	
-	public FractalPanel() {
-		zoomer = new Zoomer (this);
-	}
 
-	public static void main(String[] args) {
-		JFrame frame = new JFrame ("Fractals");
-		final FractalPanel panel = new FractalPanel();
-		frame.getContentPane().add (panel);
-		frame.addWindowListener(new WindowAdapter() {
-			@Override
-			public void windowClosing(WindowEvent arg0) {
-				System.exit(0);
-			}
-		});
-		panel.addComponentListener(new ComponentAdapter () {
-			@Override
-			public void componentResized(ComponentEvent arg0) {
-				panel.repaint();
-			}
-		});
-		panel.addMouseListener(panel.zoomer);
-		panel.addMouseMotionListener(panel.zoomer);
-		panel.addMouseWheelListener(panel.zoomer);
-		frame.addKeyListener(panel.paletteScroller);
-		frame.pack();
-		frame.setVisible(true);
+	public FractalPanel() {
+		
+		addMouseListener(zoomer);
+		addMouseMotionListener(zoomer);
+		addMouseWheelListener(zoomer);
+		//TODO why doesnt work???
+		addKeyListener(paletteScroller);
 	}
 
 	public Dimension getPreferredSize() {
@@ -52,9 +32,9 @@ public class FractalPanel extends JComponent {
 
 	public void paint(Graphics g) {
 		Dimension size = getSize ();
-		if (image == null || !conversion.getSize ().equals (size)) {
-			conversion.setSize(getSize());
-			System.out.println(conversion);
+		if (image == null || !getConversion().getSize ().equals (size)) {
+			getConversion().setSize(getSize());
+			System.out.println(getConversion());
 			int[] pixels = buildPixelsMatrix(getSize().width, getSize().height);
 			image = paletteScroller.getImage (pixels, getSize().width, getSize().height);
 		}
@@ -67,7 +47,7 @@ public class FractalPanel extends JComponent {
 		int index = 0;
 		for (int y = 0; y < height; y++) {
 			for (int x = 0; x < width; x++){
-				int colorIndex = conversion.getColorIndex(x, y);
+				int colorIndex = getConversion().getColorIndex(x, y);
 				pixels [index++] = (colorIndex << 24) | (colorIndex << 16) | (colorIndex);
 			}
 		}
@@ -77,8 +57,16 @@ public class FractalPanel extends JComponent {
 	public void setImage(Image image) {
 		this.image = image;
 	}
+
+	public void setConversion(Conversion conversion) {
+		this.conversion = conversion;
+	}
+
+	public Conversion getConversion() {
+		return conversion;
+	}
 	
-	public Image getImage () {
-		return image;
+	public KeyListener asKeyListener () {
+		return paletteScroller;
 	}
 }
